6e320526be014e400699a2d95b221fd702fb0d46
Obagade <rotimiobags@gmail.com>
2020-06-16 15:34:48 +0200

    the components layout and styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Cook with friends',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Watch a Movie',
+        completed: false
+      }
+    ]
+  }
+  
+  //Toggle complete
+  markComplete = (id) => {
+    this.setState( { todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+//Delete Todo
+delTodo = (id) => {
+  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+}
+
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+         <Header />
+         <AddTodo /> 
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     );
   }
